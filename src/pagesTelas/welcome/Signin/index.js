09b58703b48d759c6b4cbaef3880ba5bacdd3acf
RONALDOPEREIRADASILVA <eletrotecnica.ronaldo@gmail.com>
2022-09-28 19:18:40 -0300

    Criando tela login

---
+++ src/pagesTelas/welcome/Signin/index.js
@@ -0,0 +1,11 @@
+import react from "react";
+import { View , Text } from "react-native";
+
+export default function Signin() {
+    return(
+        <View>
+        <Text>Tela Bem Vindo</Text>
+        </View>
+    );
+
+}
---
+++ src/pagesTelas/welcome/index.js
@@ -0,0 +1,11 @@
+import react from "react";
+import { View , Text } from "react-native";
+
+export default function welcome() {
+    return(
+        <View>
+        <Text>Tela Bem Vindo</Text>
+        </View>
+    );
+
+}
